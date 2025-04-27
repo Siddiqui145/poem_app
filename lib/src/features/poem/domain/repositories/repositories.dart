539fd2249d97dc7964ad55108f5b9e521065be55
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-27 23:40:35 +0530

    2nd step just to define possible operations/ methods

--- lib/src/features/poem/domain/repositories/repositories.dart
+++ lib/src/features/poem/domain/repositories/repositories.dart
@@ -1,5 +1,8 @@
- 
-    abstract class PoemRepository {
-        // Future<User> getUser(String userId);
+ import 'package:poem_app/src/features/poem/domain/entities/entities.dart';
+
+// 2nd step, external api links all separated, here only possible actions
+// like we have fetch operation based on our entity structure.
+abstract class PoemRepository {
+        Future<List<PoemEntity>> fetchPoemsByAuthor(String author);
       }
     
