3b10919828c60ddb943e769c3267017c5b851bc9
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-27 23:41:23 +0530

    3rd step a center place for defining all possible actions

--- lib/src/features/poem/domain/usecases/usecases.dart
+++ lib/src/features/poem/domain/usecases/usecases.dart
@@ -1,5 +1,8 @@
- 
-    import '../repositories/repositories.dart';
+ import 'package:poem_app/src/features/poem/domain/entities/entities.dart';
+import '../repositories/repositories.dart';
+
+// 3rd step use case written for calling the repository
+// it is like a 1 structured class that shows or lists what all possible actions could be taken
 
     class GetPoemUseCase {
         final PoemRepository repository;
@@ -9,5 +12,9 @@
         // Future<User> execute(String userId) async {
         //   return userRepository.getUser(userId);
         // }
+
+        Future<List<PoemEntity>> execute(String author) {
+          return repository.fetchPoemsByAuthor(author);
+        }
       }
       
