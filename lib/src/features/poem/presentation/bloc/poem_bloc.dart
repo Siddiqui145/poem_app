697a4cb23d54bc3483c76b2a9d847eef90a1c141
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-30 13:01:36 +0530

    9th step for final bloc, using the usecase for defined tasks that could be done

---
+++ lib/src/features/poem/presentation/bloc/poem_bloc.dart
@@ -0,0 +1,27 @@
+// presentation/bloc/poem_bloc.dart
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poem_app/src/features/poem/domain/usecases/usecases.dart';
+import 'package:poem_app/src/features/poem/presentation/bloc/poem_state.dart';
+
+
+part 'poem_event.dart';
+//part 'poem_state.dart';
+
+class PoemBloc extends Bloc<PoemEvent, PoemState> {
+  final GetPoemUseCase fetchPoems;
+
+  PoemBloc({required this.fetchPoems}) : super(PoemInitial()) {
+    on<FetchPoemsEvent>(_onFetchPoemsEvent);
+  }
+
+  Future<void> _onFetchPoemsEvent(FetchPoemsEvent event, Emitter<PoemState> emit) async {
+    emit(PoemLoading());
+    try {
+      final poems = await fetchPoems.execute(event.author);
+      emit(PoemLoaded(poems));
+    } catch (e) {
+      emit(PoemError(e.toString()));
+    }
+  }
+}
