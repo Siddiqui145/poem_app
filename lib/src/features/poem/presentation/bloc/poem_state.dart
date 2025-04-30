17cddef3601812200b828d0a6035fa98b0e8c815
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-30 13:00:54 +0530

    8th step for listing all states => initial, loading

---
+++ lib/src/features/poem/presentation/bloc/poem_state.dart
@@ -0,0 +1,18 @@
+import 'package:poem_app/src/features/poem/domain/entities/entities.dart';
+
+
+abstract class PoemState {}
+
+class PoemInitial extends PoemState {}
+
+class PoemLoading extends PoemState {}
+
+class PoemLoaded extends PoemState {
+  final List<PoemEntity> poems;
+  PoemLoaded(this.poems);
+}
+
+class PoemError extends PoemState {
+  final String message;
+  PoemError(this.message);
+}
