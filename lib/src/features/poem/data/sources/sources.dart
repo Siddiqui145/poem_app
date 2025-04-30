a4d6536e5870ee66dcf6ecb3b60328017e1dda76
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-30 12:51:19 +0530

    5th step API handled for fetch & getting data

--- lib/src/features/poem/data/sources/sources.dart
+++ lib/src/features/poem/data/sources/sources.dart
@@ -1,5 +1,27 @@
  
-    class  PoemRemoteDataSource {
+    import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:poem_app/src/features/poem/data/models/models.dart';
+
+class  PoemRemoteDataSource {
         // Make API call to fetch data and return object.
         // ...
+    // 5th Step here we make the final api call using http package, and model is used and actual data is fetched
+    final http.Client client;
+
+    PoemRemoteDataSource({required this.client});
+
+    Future<List<PoemModel>> getPoemsByAuthor(String author) async{
+        final url = Uri.parse("https://poetrydb.org/author/$author");
+        final response = await client.get(url);
+
+        if(response.statusCode == 200){
+            final List<dynamic> decodedJson = json.decode(response.body);
+            return decodedJson.map((e) => PoemModel.fromJson(e)).toList();
+        }
+        else {
+            throw Exception("Failed to load peoms");
+        }
+    }
     }
