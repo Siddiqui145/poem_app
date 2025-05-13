1d0e61fa5c04a0c59e687b519aa11179c6abf40e
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-05-13 13:04:45 +0530

    added dropdown for dynamism in authors

--- lib/src/features/poem/presentation/pages/poem_screen.dart
+++ lib/src/features/poem/presentation/pages/poem_screen.dart
@@ -5,30 +5,38 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poem_app/src/features/poem/presentation/bloc/poem_state.dart';
 import '../bloc/poem_bloc.dart';
 
-class PoemScreen extends StatelessWidget {
+class PoemScreen extends StatefulWidget {
   const PoemScreen({super.key});
+
+  @override
+  State<PoemScreen> createState() => _PoemScreenState();
+}
+
+class _PoemScreenState extends State<PoemScreen> {
+  String? selectedAuthor;
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<PoemBloc>().add(FetchAuthorsEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Emily Dickinson Poems'),
+      appBar: AppBar(title: Text( selectedAuthor!=null ? "$selectedAuthor's Poems" : 'Poems'),
       backgroundColor: Colors.lightBlue.shade100,
       actions: [
-        IconButton(
-        onPressed: () {
-          context.read<PoemBloc>().add(FetchPoemsEvent('Emily Dickinson'));
-        },
-        icon: const Icon(Icons.refresh),
-      ),
+      
 
         IconButton(
         onPressed: () {
-          context.read<PoemBloc>().add(ClosePoemsEvent());
+          context.read<PoemBloc>().add(FetchAuthorsEvent());
         },
         icon: const Icon(Icons.close),
       ),
-
       ],),
+
       body: BlocBuilder<PoemBloc, PoemState>(
         builder: (context, state) {
           if (state is PoemLoading) {
@@ -36,6 +44,32 @@ class PoemScreen extends StatelessWidget {
           }
           else if (state is PoemInitial){
             return const Center(child: Text("Initial state"));
+          }
+          else if (state is AuthorsLoaded){
+            return Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  DropdownButton<String>(
+                    value: selectedAuthor,
+                    hint: const Text('Select Author'),
+                    isExpanded: true,
+                    items: state.authors.map((author) {
+                      return DropdownMenuItem<String>(
+                        value: author,
+                        child: Text(author),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() => selectedAuthor = value);
+                      if (value != null) {
+                        context.read<PoemBloc>().add(FetchPoemsEvent(value));
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
           }
            else if (state is PoemLoaded) {
             return ListView.builder(
@@ -54,7 +88,6 @@ class PoemScreen extends StatelessWidget {
           return const Center(child: Text('Press the button to fetch poems'));
         },
       ),
-      
     );
   }
 }
